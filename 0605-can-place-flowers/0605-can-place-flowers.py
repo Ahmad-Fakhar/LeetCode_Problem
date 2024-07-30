ad4dc171ca879fa6d2987b978d7c2c4d1c677180
Ahmad Fakhar <155258276+Ahmad-Fakhar@users.noreply.github.com>
2024-07-30 23:57:21 +0500

    Time: 139 ms (21.1%), Space: 16.9 MB (19.86%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -6,6 +6,7 @@ class Solution:
             if arr[i] == 0 and arr[i - 1] == 0 and arr[i + 1] == 0:
                 arr[i] = 1
                 n -= 1
-
+                if n <= 0:
+                    return True
         
         return n <= 0
